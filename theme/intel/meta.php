b6f54280bf4a9a1bb84e558d5a6f53b2b4735207
Nadav Kavalerchik <nadavkav@gmail.com>
2010-07-25 18:52:10 +0300

    IE8 support , rtl CSS patches , resized GIFs

--- theme/intel/meta.php
+++ theme/intel/meta.php
@@ -1,6 +1,10 @@
+<!-- added support for IE8 (nadavkav) -->
+<!--[if IE 8]>
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/intel/styles_ie8.css" />
+<![endif]-->
 <!--[if IE 7]>
-    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/custom_corners/styles_ie7.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/intel/styles_ie7.css" />
 <![endif]-->
 <!--[if IE 6]>
-    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/custom_corners/styles_ie6.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/intel/styles_ie6.css" />
 <![endif]-->
